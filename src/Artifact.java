fd405700c835fb8d284ef1d580c0dc40bd2ea29c
Leonid <airspacece@mail.ru>
2023-03-02 21:37:46 +1000

    123

--- src/Artifact.java
+++ src/Artifact.java
@@ -2,7 +2,6 @@ public class Artifact extends GameObject {
 
     private String name;
     private String rarity;
-    private String eee;
 
     public Artifact() {
         this.name = "Stone with eternity time";
